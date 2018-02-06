bbed2ed43e6e6d881311a4a462984b2909161fc2
Ky Hyeun Lee <klee@quid.com>
2018-02-06 10:32:32 -0800

    save in home directory instead of inside of src folder

--- src/links.js
+++ src/links.js
@@ -2,23 +2,24 @@
 
 var fs = require('fs-extra');
 var path = require('path');
+var homedir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
-var linksPath = path.resolve(__dirname, 'links.json');
+var linksPath = path.join(homedir, '.config/wml', 'links.json');
 
 module.exports.data = [];
 
 module.exports.load = function() {
-	var links;
+  var links;
 
-	try {
-		links = fs.readJsonSync(linksPath);
-	} catch (err) {
-		links = {};
-	}
+  try {
+    links = fs.readJsonSync(linksPath);
+  } catch (err) {
+    links = {};
+  }
 
-	module.exports.data = links;
+  module.exports.data = links;
 };
 
 module.exports.save = function() {
-	fs.outputJsonSync(linksPath, module.exports.data);
+  fs.outputJsonSync(linksPath, module.exports.data);
 };
